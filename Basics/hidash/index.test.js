168e2ef7764a64803812eb8fcefc484b3182c0ef
dariusdv <ddariusn@gmail.com>
2020-06-20 00:13:21 +0300

    Mocha test update

--- Basics/hidash/index.test.js
+++ Basics/hidash/index.test.js
@@ -1,17 +1,25 @@
 const assert = require('assert');
-
 const { forEach, map } = require('./index');
 
-const test = (desc, fn) => {
-	console.log('----', desc);
-	try {
-		fn();
-	} catch (err) {
-		console.log(`ERROR: ${err.message}`);
-	}
-};
+// const test = (desc, fn) => {
+// 	console.log('----', desc);
+// 	try {
+// 		fn();
+// 	} catch (err) {
+// 		console.log(`ERROR: ${err.message}`);
+// 	}
+// };
 
-test('The forEach function', () => {
+// test('The forEach function', () => {
+// 	let sum = 0;
+// 	forEach([ 1, 2, 3 ], (value) => {
+// 		sum += value;
+// 	});
+
+// 	assert.strictEqual(sum, 6, 'Expected summing array to equal 6');
+// });
+
+it('The forEach function', () => {
 	let sum = 0;
 	forEach([ 1, 2, 3 ], (value) => {
 		sum += value;
@@ -20,7 +28,7 @@ test('The forEach function', () => {
 	assert.strictEqual(sum, 6, 'Expected summing array to equal 6');
 });
 
-test('The map function', () => {
+it('The map function', () => {
 	const result = map([ 1, 2, 3 ], (value) => {
 		return value * 2;
 	});
@@ -32,3 +40,16 @@ test('The map function', () => {
 	// assert.strictEqual(result[1], 4, `Expected to find 4, but found ${result[1]}`);
 	// assert.strictEqual(result[2], 6, `Expected to find 6, but found ${result[2]}`);
 });
+
+// test('The map function', () => {
+// 	const result = map([ 1, 2, 3 ], (value) => {
+// 		return value * 2;
+// 	});
+
+// 	assert.deepStrictEqual(result, [ 2, 4, 6 ]);
+
+// 	// Another way to test:
+// 	// assert.strictEqual(result[0], 2, `Expected to find 2, but found ${result[0]}`);
+// 	// assert.strictEqual(result[1], 4, `Expected to find 4, but found ${result[1]}`);
+// 	// assert.strictEqual(result[2], 6, `Expected to find 6, but found ${result[2]}`);
+// });
